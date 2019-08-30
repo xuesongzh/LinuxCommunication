90e68054ce00c13f989a57d89e66e8e4755e1baf
kdvictor <huanliu_uestc@163.com>
2019-08-31 06:35:52 +0800

    nginx源码学习，终端进程之间的关系（3）

---
+++ LinuxCommunication/nginx/test.c
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <unistd.h>
+
+#include<signal.h>
+
+int main()
+{
+
+	pid_t pid;
+
+	printf("干你Y的\n");
+
+	// //系统函数，设置某个信号来的时候处理，SIG_IGN忽略信号标志，请操作系统不要用缺省的信号处理方式来处理
+	// signal(SIGHUP, SIG_IGN);
+
+	pid = fork(); //系统函数，创建新进程。后面的代码子进程和父进程都会往后执行
+	if(pid < 0)
+	{
+		printf("failed to fork son process.\n");
+	}
+	else if(pid == 0)//子进程
+	{
+		printf("son process......\n");
+		setsid(); //子进程创建session有效
+
+		for (;;)
+		{
+			sleep(1);
+			printf("子进程休息一秒\n");
+		}
+
+		return 0;
+	}
+	else
+	{
+		for (;;)
+		{
+			sleep(1);
+			printf("父进程休息一秒\n");
+		}
+	}
+
+	// setsid();//新建一个session,但是进程组组长调用无效
+
+
+	// for(;;)
+	// {
+	// 	sleep(1);
+	// 	printf("休息一秒\n");
+	// }
+
+	printf("程序退出\n");
+
+	return 0;
+}
---
+++ LinuxCommunication/nginx/testSrc.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+
+	printf("干你Y的\n");
+
+	for(;;)
+	{
+		sleep(1);
+		printf("休息一秒\n");
+	}
+
+	printf("程序退出\n");
+
+	return 0;
+}
