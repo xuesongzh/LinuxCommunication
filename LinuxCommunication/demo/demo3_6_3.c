d0752fea2d45f1646d95e1a755d581b25650e58d
kdvictor <huanliu_uestc@163.com>
2019-09-29 06:37:29 +0800

    fork函数进一步认识

---
+++ LinuxCommunication/demo/demo3_6_3.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+	fork(); //假定fork()成功
+	fork();
+
+	//((fork() && fork()) || (fork() && fork()));
+	//printf("the max number of process=%ld!\n", sysconf(_SC_CHILD_MAX));
+
+	while (1)
+	{
+		sleep(1);
+		printf("sleep 1 second, pid=%d!\n", getpid());
+	}
+
+	printf("exit!\n");
+	return 0;
+}
