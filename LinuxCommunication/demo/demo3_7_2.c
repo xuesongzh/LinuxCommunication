93b4c01bbaea9ae8a7d3f1ee0ae5905b5c66b884
kdvictor <huanliu_uestc@163.com>
2019-09-30 06:14:40 +0800

    标准输入输出描述符

---
+++ LinuxCommunication/demo/demo3_7_2.c
@@ -0,0 +1,18 @@
+﻿/*标准输入输出*/
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* const* argv)
+{
+	printf("start process!\n");
+	write(STDOUT_FILENO, "AAAAAAbb", 6); //标准输出，向屏幕输出
+
+	while (1)
+	{
+		sleep(1);
+		printf("sleep 1 second, pid=%d.\n", getpid());
+	}
+
+	printf("exit!\n");
+	return 0;
+}
