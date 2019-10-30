1270e9628695784e097624ef22123c4f8facf54b
kdvictor <huanliu_uestc@163.com>
2019-10-30 08:50:28 +0800

    日志初始化完成

--- LinuxCommunication/nginx/_include/ngx_defs.h
+++ LinuxCommunication/nginx/_include/ngx_defs.h
@@ -14,6 +14,29 @@
 #ifndef __NGINX_DEFS_H__
 #define __NGINX_DEFS_H__
 
+/*---------------日志相关-----------------*/
+#define NGX_LOG_PATH "logs/error1.log" //缺省log路径
+
+//日志等级：0级别最高，8级最低
+#define NGX_LOG_STDERR      0 //严重错误，不输出到日志文件，直接打印到控制台或者屏幕上
+#define NGX_LOG_EMERG       1 //紧急[meerg]
+#define NGX_LOG_ALERT       2 //警戒[alert]
+#define NGX_LOG_CRIT        3 //严重[crit]
+#define NGX_LOG_ERR         4 //错误[error] ：常用
+#define NGX_LOG_WARNING     5 //警告[warning]：常用
+#define NGX_LOG_NOTICE      6 //注意[notice]
+#define NGX_LOG_INFO        7 //信息[info]
+#define NGX_LOG_DEBUG       8 //调试[debug]
+
+
+
+
+
+
+
+
+
+
 //delete pointer
 #ifndef DEL_PTR
 #define DEL_PTR(ptr)        \
---
+++ LinuxCommunication/nginx/app/ngx_log.cxx
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <fcntl.h> //open
+#include <unistd.h> //STDERR_FILENO等
+#include <stdlib.h> 
+#include <errno.h> //errno
+
+
+#include "ngx_log.h"
+#include "ngx_configer.h"
+#include "ngx_defs.h"
+#include "ngx_datamodel.h"
+
+ngx_log_t ngx_log;
+
+void ngx_log_stderr(int err, const char* fmt, ...)
+{
+
+}
+
+
+//日志初始化，将日志文件打开
+void ngx_log_init()
+{
+    u_char* pLogName = nullptr;
+
+    //从配置文件中读取和日志相关的配置信息
+    Configer* pConfiger = Configer::GetInstance();
+    pLogName = (u_char*)pConfiger->GetContentByName("LogPath");
+    if(pLogName == nullptr)
+    {
+        //没有读取成功，需要设置缺省的路径
+        pLogName = (u_char*) NGX_LOG_PATH;
+    }
+
+    ngx_log.log_level = pConfiger->GetNumberByName("LogLevel", NGX_LOG_NOTICE); //默认等级6
+
+    //只写打开|追加到末尾|文件不存在则创建【这个需要跟第三参数指定文件访问权限】
+    //mode = 0644：文件访问权限，6: 110, 4: 100：【用户：读写， 用户所在组：读，其他：读】
+    ngx_log.fd = open((const char*) pLogName, O_WRONLY|O_APPEND|O_CREAT, 0644);
+
+    if(ngx_log.fd == -1) 
+    {
+        //如果有错误，就直接定位到标准错误上去
+        ngx_log_stderr(errno, "[alert] could not open error log file: open() \"%s\" failed", pLogName);
+        ngx_log.fd = STDERR_FILENO;
+
+    }
+    
+}
