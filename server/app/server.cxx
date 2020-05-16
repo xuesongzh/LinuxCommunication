a55708760efc8490d6d59b98c64c2b33b0edbf9f
kdvictor <huanliu_uestc@163.com>
2020-05-16 22:06:00 +0800

    代码调整

--- server/app/server.cxx
+++ server/app/server.cxx
@@ -14,11 +14,34 @@ char* pNewEnviron = nullptr;
 int EnvironLength = 0;
 pid_t ser_pid;
 
+static void FreeSource();
+
 int main(int argc, char* const* argv)
 {
+	int exitCode = 0;
 	ser_pid = getpid(); //获取进程ID
 	pArgv = const_cast<char**>(argv);
-	//将环境变量搬走
+	const char* masterProcessTitle = nullptr; //主进程标题
+
+	//配置文件加载
+	SerConfiger* pConfiger = SerConfiger::GetInstance();
+	if (nullptr == pConfiger)
+	{
+		SER_LOG_STDERR(errno, "create configer failed!");
+		exitCode = 1;
+		goto lblexit;
+	}
+	if (true != pConfiger->Load("server.conf"))
+	{
+		SER_LOG_STDERR(errno, "load configer content failed!");
+		exitCode = 2; //找不到文件
+		goto lblexit;
+	}
+
+	//日志系统初始化
+	SER_LOG_INIT();
+
+	//将环境变量搬走：为了设置进程标题
 	for (int i = 0; environ[i]; ++i)
 	{
 		EnvironLength += strlen(environ[i]) + 1; //环境变量长度，+1是因为'\0'的存在
@@ -26,29 +49,13 @@ int main(int argc, char* const* argv)
 	pNewEnviron = new char[EnvironLength];
 	memset(pNewEnviron, 0, EnvironLength);
 	MoveEnviron(pNewEnviron);
-	//配置文件加载
-	SerConfiger* pConfiger = SerConfiger::GetInstance();
-	if (nullptr == pConfiger)
-	{
-		SER_LOG_STDERR(errno, "create configer failed!");
-		exit(1);
-	}
-	if (true != pConfiger->Load("server.conf"))
-	{
-		SER_LOG_STDERR(errno, "load configer content failed!");
-		exit(1);
-	}
-
-	SER_LOG_INIT();
-	SER_LOG(SER_LOG_DEBUG,1,"test debug!");
-	SER_LOG(SER_LOG_INFO,2,"test info!");
-
 	//设置主进程标题
-	const char* masterProcessTitle = pConfiger->GetString("MasterProcessTitle");
+	masterProcessTitle = pConfiger->GetString("MasterProcessTitle");
 	if (nullptr == masterProcessTitle)
 	{
 		SER_LOG_STDERR(errno, "get master process title failed!");
-		exit(1);
+		exitCode = 1;
+		goto lblexit;
 	}
 	SetProcessTitle(masterProcessTitle);
 
@@ -58,10 +65,21 @@ int main(int argc, char* const* argv)
 	// 	printf("sleep 1 second!\n");
 	// }
 
+lblexit:
+	FreeSource();
 	printf("程序退出!\n");
+	return exitCode;
+}
 
-	//程序退出的时候要释放内存
-	DEL_ARRAY(pNewEnviron);
+static void FreeSource()
+{
+	//释放新的环境变量所在内存
+	DEL_ARRAY(pNewEnviron);	
 
-	return 0;
+	//关闭日志文件
+	if(ser_log.mFd != STDERR_FILENO && ser_log.mFd != -1)
+	{
+		close(ser_log.mFd);
+		ser_log.mFd = -1; //标记下，防止再次被close
+	}
 }
